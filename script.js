64d4d27bdc7fb1bf5d6f1682d1a971b303c13f9b
fran060606 <113519511+fran060606@users.noreply.github.com>
2023-04-11 15:55:18 +0200

    post, get

---
+++ script.js
@@ -0,0 +1,48 @@
+const tasksElement = document.getElementById("tasks");
+
+async function getTasks() {
+    const response = await fetch("http://localhost:3000/tasks", {
+        method: "GET"
+    });
+    const json = await response.json();
+
+    renderTasks(json);
+}
+
+async function addTask() {
+    const title = prompt("Was soll im Task stehen?");
+
+    const response = await fetch("http://localhost:3000/tasks", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({
+            title: title
+        })
+    });
+
+    getTasks();
+}
+
+
+
+
+
+function renderTasks(tasks) {
+    tasksElement.replaceChildren();
+
+    tasks.forEach(function (item) {
+        const liElement = document.createElement("li");
+        liElement.innerText = item.title;
+
+        const deleteButton = document.createElement("button");
+        deleteButton.innerText = "Delete";
+        deleteButton.onclick = function () {
+            deleteTask(item.id);
+        };
+
+        liElement.append(deleteButton);
+        tasksElement.append(liElement);
+    });
+}
